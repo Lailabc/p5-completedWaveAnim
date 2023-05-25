f41b304781d14fbd6ddd08f28008814931a18ab8
Lailabc <115160711+Lailabc@users.noreply.github.com>
2023-05-25 15:31:26 +0100

    Update sketch.js

--- base-p5-project-main/js/sketch.js
+++ base-p5-project-main/js/sketch.js
@@ -8,34 +8,37 @@ let frequency;
 let maxAngle;
 function setup() {
     amplitude = windowHeight / 10;
-    frequency = windowWidth / 0.5;
+    frequency = windowWidth / 0.05;
     maxAngle = (windowWidth / frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
 
 
 }
 function draw() {
-    background(0);
-    noStroke();
-    let length = 20;
 
+    noStroke();
+    let length = 10;
+
+    background(10,10);
+    
     while (theta < maxAngle + offset) {
-        // amplitude = sin(theta - offset)* (windowHeight/2);
-        amplitude = ((theta-offset)/maxAngle)* (windowHeight/2)
+        amplitude = sin(theta - offset)* (windowHeight/2);
+        // amplitude = ((theta-offset)/maxAngle)* (windowHeight/2)
         for (i = length; i > 0; i--) {
             y = sin(theta - (i * 0.05)) * amplitude;
-            fill(255 - (255/length * i), 0, 255 );
+            fill(255- (255/length * i), 255, 255);
             // ellipse(x, y + height / 2, 16);
-            square(x,height/2 + y,16);
+            ellipse(x,mouseY,mouseY/2, mouseX/2);
         }
-        for (i = length; i > 0; i--) {
-            y = cos(theta - (i * 0.05)) * amplitude;
-            fill(255 - (255/length * i), 255, 0 );
-            // ellipse(x, y + height / 2, 16);
-            square(x,height/2 + y,16);
-        }
+        // for (i = length; i > 0; i--) {
+        //     y = cos(theta - (i * 0.05)) * amplitude;
+        //     fill(255 - (255/length * i), 255, 255 );
+        //     // ellipse(x, y + height / 2, 16);
+        //     ellipse(x,mouseY,mouseY/2, mouseX/2);
+        // }
         theta += 0.2;
-        x = ((theta - offset) / maxAngle) * windowWidth;
+        // x = ((theta - offset) / maxAngle) * windowWidth;
+        x= mouseX  * ((theta - offset) / maxAngle) ;
        
     }
     offset += increment;
